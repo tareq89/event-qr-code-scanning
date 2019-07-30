338fe6c7c915875aaeb1d4de0aedc21f909ecc25
tareq89 <tareqaziz.aust.cse@gmail.com>
2019-07-30 22:01:15 +0600

    basic code done

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,110 @@
 <template>
   <section class="container">
     <div>
-      <logo/>
-      <h1 class="title">
-        qr-code-scan
-      </h1>
-      <h2 class="subtitle">
-        My spectacular Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
+      <qrcode-stream @decode="onDecode" @init="onInit" />
+      <span v-if="error" class="error">{{ error }}</span>
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import { QrcodeStream } from 'vue-qrcode-reader'
+
+function isUrl (data) {
+  return true
+}
+
+function callMatchUrl (url) {
+  switch (url) {
+    case 1:
+      return Promise.resolve({
+        isPaid: 1,
+        isAttended: 0,
+        approve_url: 'https://laravel.com.bd'
+      })
+    case 1:
+      return Promise.resolve({
+        isPaid: 1,
+        isAttended: 1
+      })
+    default:
+      return Promise.resolve({
+        isPaid: 0,
+        isAttended: 0
+      })
+  }
+}
+
+function callApproveUrl (url) {
+  return Promise.resolve({})
+}
 
 export default {
   components: {
-    Logo
+    QrcodeStream
+  },
+  data () {
+    return {
+      result: '',
+      error: '',
+      info: '',
+      showConfirmAttendance: false
+    }
+  },
+  methods: {
+    onDecode (result) {
+      this.result = result
+      if (isUrl(result)) {
+        callMatchUrl(result)
+          .then(result => {
+            if (result.isPaid) {
+              if (result.isAttended) {
+                this.info = "You have allready confirmed your attendance"
+              } else {
+                this.showConfirmAttendance = true
+              }
+            } else {
+              this.info = "You have not paid for the event yet"
+            }
+          })
+      } 
+    },
+    confirmAttendance () {
+      callApproveUrl(this.result)
+        .then(() => {
+          alert('Your attendance has been confirmed')
+        })
+        .catch(() => {
+          alert('CONFIRMATION FAILED!')
+        })
+        .finally(() => {
+          this.resetState()
+        })
+    },
+    resetState () {
+      this.info = null
+      this.showConfirmAttendance = false
+      this.result = null
+    },
+    async onInit (promise) {
+      try {
+        await promise
+      } catch (error) {
+        if (error.name === 'NotAllowedError') {
+          this.error = "ERROR: you need to grant camera access permisson"
+        } else if (error.name === 'NotFoundError') {
+          this.error = "ERROR: no camera on this device"
+        } else if (error.name === 'NotSupportedError') {
+          this.error = "ERROR: secure context required (HTTPS, localhost)"
+        } else if (error.name === 'NotReadableError') {
+          this.error = "ERROR: is the camera already in use?"
+        } else if (error.name === 'OverconstrainedError') {
+          this.error = "ERROR: installed cameras are not suitable"
+        } else if (error.name === 'StreamApiNotSupportedError') {
+          this.error = "ERROR: Stream API is not supported in this browser"
+        }
+      }
+    }
   }
 }
 </script>
@@ -55,5 +138,10 @@ export default {
 .links
 {
   padding-top: 15px;
+}
+.error {
+  background-color: red;
+  color: white;
+  font-size: 1em;
 }
 </style>
